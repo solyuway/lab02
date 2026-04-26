1c8bfb71064657a7188b3eb734c22f1d5bdd4bb3
{GITHUB_USERNAME} <{GITHUB_EMAIL}>
2026-04-26 11:57:17 -0400

    Apply Mozilla code style with clang-format

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <string>
-int main() {
-std::string name;
-std::cout << "Введите имя: ";
-//Запрашиваем user name.
-std::cin >> name;
-//Выводим приветствие
-std::cout << "Hello world from " << name << std::endl;
-return 0;
+int
+main()
+{
+  std::string name;
+  std::cout << "Enter name: ";
+  // Запрашиваем имя пользователя
+  std::cin >> name;
+  // Выводим приветствие
+  std::cout << "Hello world from " << name << std::endl;
+  return 0;
 }
