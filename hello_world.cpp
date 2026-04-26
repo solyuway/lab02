5cb3ce63db1a15c8d027b753d0ead31ffb95c43d
{GITHUB_USERNAME} <{GITHUB_EMAIL}>
2026-04-26 11:16:16 -0400

    Add comments

--- hello_world.cpp
+++ hello_world.cpp
@@ -3,7 +3,9 @@
 int main() {
 std::string name;
 std::cout << "Enter name: ";
+//Запрашиваем имя пользователя
 std::cin >> name;
+//Выводим приветствие
 std::cout << "Hello world from " << name << std::endl;
 return 0;
 }
